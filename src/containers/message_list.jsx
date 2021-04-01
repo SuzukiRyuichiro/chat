edb6792875d0d7c7ee5bfeeae8407710ea8ce4e7
Ryuichiro Suzuki <dragon.aka.scooter@gmail.com>
2021-04-01 12:02:51 +0900

    added an action

---
+++ src/containers/message_list.jsx
@@ -0,0 +1,10 @@
+import  React from 'react';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import { setMessages } from '../actions'
+
+class MessageList extends from React.Component {
+
+}
+
+export default MessageList;
