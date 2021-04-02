4c01ad57b464f7f2af015b1e3b06d39a8687e667
Ryuichiro Suzuki <dragon.aka.scooter@gmail.com>
2021-04-02 14:27:43 +0900

    added bootstrap

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -4,7 +4,13 @@ import MessageList from '../containers/message_list'
 const App = () => {
   return (
     <div className="app">
-      <MessageList />
+      <div className="left-screen">
+        <div className="bg-primary">
+        </div>
+      </div>
+      <div className="right-screen">
+        <MessageList />
+      </div>
     </div>
   );
 };
