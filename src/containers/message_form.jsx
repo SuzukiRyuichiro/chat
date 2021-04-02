13b2c035fbd2b598fb11de858ee62aea700a2d3f
Ryuichiro Suzuki <dragon.aka.scooter@gmail.com>
2021-04-02 15:39:30 +0900

    form now updateS

--- src/containers/message_form.jsx
+++ src/containers/message_form.jsx
@@ -1,11 +1,34 @@
 import React from 'react';
 
 export default class MessageForm extends React.Component {
+  constructor(props){
+    super(props);
+    console.log('constructoring');
+    this.state = {
+      value: 'hello'
+    };
+  }
+
+  handleSubmit = () => {
+
+  }
+
+  handleChange = (event) => {
+    console.log(event.target.value);
+    this.setState({
+      value: event.target.value
+    })
+  }
+
   render(){
     return(
-      <div className="message-form">
-        hey
-      </div>
+      <form onSubmit={this.handleSubmit} className="message-form">
+        <label>
+          Name:
+          <input type="text" value={this.state.value} onChange={this.handleChange} />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
     )
   }
 }
