c606941e15e761ae795858f5b35500879197d836
Ryuichiro Suzuki <dragon.aka.scooter@gmail.com>
2021-04-02 15:19:35 +0900

    new action

--- src/actions/index.js
+++ src/actions/index.js
@@ -5,3 +5,10 @@ export function setMessages(messages) {
     payload: messages
   }
 }
+
+export function createMessage(channel, author, content) {
+  return {
+    type: 'SEND_MESSAGE',
+    payload: {channel: channel, author: author, content: content}
+  }
+}
