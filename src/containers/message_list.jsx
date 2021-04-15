3a68c2003822b37bfcb5b44012944b027a173f97
Ryuichiro Suzuki <dragon.aka.scooter@gmail.com>
2021-04-15 10:50:08 +0900

    past tense fixture

--- src/containers/message_list.jsx
+++ src/containers/message_list.jsx
@@ -44,7 +44,7 @@ class MessageList extends React.Component {
         return(
           <div className="message-list">
             <h3>No channel selected </h3>
-            <p className="text-muted">(Also, it may take a while if you just open the app. Heroku dyno is probably waking up <i class="devicon-heroku-plain"></i>)</p>
+            <p className="text-muted">(Also, it may take a while if you just opened the app. Heroku dyno is probably waking up <i class="devicon-heroku-plain"></i>)</p>
           </div>
         )
       }
